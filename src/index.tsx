2433bc8c09a4d993a9072d9af15c3b0b45d9acb8
Edwin Carl Flores <edwincarlflores@gmail.com>
2023-09-04 06:44:37 +1200

    Add jsx and htmx support

---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
+import * as elements from "typed-html";
+
+const app = new Elysia()
+  .use(html())
+  .get("/", ({ html }) =>
+    html(
+      <BaseHtml>
+        <body>
+          <button hx-post="/clicked" hx-swap="outerHTML">
+            Click Me!
+          </button>
+        </body>
+      </BaseHtml>
+    )
+  )
+  .post("/clicked", () => <div>I'm from the server</div>)
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
+
+const BaseHtml = ({ children }: elements.Children) => `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>THE BETH STACK</title>
+  <script src="https://unpkg.com/htmx.org@1.9.5"></script>
+  <link href="/styles.css" rel="stylesheet">
+</head>
+${children}
+`;
